88107ba5ceac18d3d01122b294def74f419827f8
furwadiallone-jpg <furwadiallone@gmail.com>
2026-05-13 23:04:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -54,7 +54,7 @@ app.use(express.static(uploadDir));
 
 // route utama -> kirim file home.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "Panelv2.html"));
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
 // API daftar file
